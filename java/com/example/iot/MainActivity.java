84b91fcecc0e24b8472c8c7221cee9971662d689
Nattapongnani <59525702+Nattapongnani@users.noreply.github.com>
2023-02-13 11:52:44 +0700

    update2

--- java/com/example/iot/MainActivity.java
+++ java/com/example/iot/MainActivity.java
@@ -37,6 +37,11 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TEMPERATURE_URL = "https://api.thingspeak.com/channels/2014343/field/1/last.json";
     private static final String HUMIDITY_URL = "https://api.thingspeak.com/channels/2014343/field/2/last.json";
+
+    //swtich
+    private static final String SWITCH1_URL = "https://api.thingspeak.com/update.json?api_key=P1KA5YJ8X7NYA6Y3&field3=";
+    private static final String SWITCH2_URL = "https://api.thingspeak.com/update.json?api_key=P1KA5YJ8X7NYA6Y3&field4=";
+
     //add
     private static final String TEMPERATURE_GRAPH_URL = "https://thingspeak.com/channels/2014343/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15";
 
@@ -106,6 +111,7 @@ public class MainActivity extends AppCompatActivity {
             }
             try{
                 JSONObject channel = (JSONObject) new JSONTokener(response).nextValue();
+                //tempratureView
                 if (channel.has("field1")){
                     String status = channel.getString("field1");
                     if(!status.equals("null")){
@@ -115,6 +121,7 @@ public class MainActivity extends AppCompatActivity {
                     //String message = getText(R.string.temperature) + " " + channel.getString("field1") + " " + getText(R.string.celsius);
                     //temperatureView.setText(message);
                 }
+                //HumidityView
                 if(channel.has("field2")){
                     String status = channel.getString("field2");
                     if(!status.equals("null")){
@@ -124,6 +131,29 @@ public class MainActivity extends AppCompatActivity {
                     //String message = getText(R.string.humdity) + " " + channel.getString("field2") + " " + getText(R.string.percent);
                     //humidityView.setText(message);
                 }
+                //Swtich 1
+                if(channel.has("field3")){
+                    String status = channel.getString("field3");
+                    String message;
+                    if(!status.equals("1")){
+                        message = getText(R.string.on1);
+                    } else {
+                        message = getText(R.string.off1);
+                    }
+                    Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+                }
+                //Switch 2
+                if(channel.has("field4")){
+                    String status = channel.getString("field4");
+                    String message;
+                    if(!status.equals("1")){
+                        message = getText(R.string.on2);
+                    } else {
+                        message = getText(R.string.off2);
+                    }
+                    Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+                }
+
             } catch(JSONException e){
                 e.printStackTrace();
             }
@@ -131,22 +161,51 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onSW1Clicked(View v){
+        String FIELD3_URL;
         boolean on = ((Switch) v).isChecked();
-
         if(on){
-            Toast.makeText(getApplicationContext(), R.string.on1, Toast.LENGTH_SHORT).show();
+            FIELD3_URL = SWITCH1_URL + "1";
+            //Toast.makeText(getApplicationContext(), R.string.on1, Toast.LENGTH_SHORT).show();
         }else {
-            Toast.makeText(getApplicationContext(), R.string.off1, Toast.LENGTH_SHORT).show();
+            FIELD3_URL = SWITCH1_URL + "0";
+            //Toast.makeText(getApplicationContext(), R.string.off1, Toast.LENGTH_SHORT).show();
         }
+        FetchThingspeakTask field3 = new FetchThingspeakTask();
+        field3.execute(FIELD3_URL);
+        disableSwitch(15000);
     }
 
     public void onSW2Clicked(View v){
+        String FIELD4_URL;
         boolean on = ((Switch) v).isChecked();
-
         if(on){
-            Toast.makeText(getApplicationContext(), R.string.on2, Toast.LENGTH_SHORT).show();
+            FIELD4_URL = SWITCH2_URL + "1";
+            //Toast.makeText(getApplicationContext(), R.string.on1, Toast.LENGTH_SHORT).show();
         }else {
-            Toast.makeText(getApplicationContext(), R.string.off2, Toast.LENGTH_SHORT).show();
+            FIELD4_URL = SWITCH2_URL + "0";
+            //Toast.makeText(getApplicationContext(), R.string.off1, Toast.LENGTH_SHORT).show();
         }
+        FetchThingspeakTask field4 = new FetchThingspeakTask();
+        field4.execute(FIELD4_URL);
+        disableSwitch(15000);
+    }
+
+    private void disableSwitch(int delay_ms){
+        final Switch Switch1View = finaViewById(R.id.sw1);
+        final Switch Switch2View = finaViewById(R.id.sw2);
+        Switch1View.setEnabled(false);
+        Switch1View.postDelayed(new Runnable() {
+            @Override
+            public void run(){
+                Switch1View.setEnabled(true);
+            } 
+        }, delay_ms)
+        Switch2View.setEnabled(false);
+        Switch2View.postDelayed(new Runnable() {
+            @Override
+            public void run(){
+                Switch2View.setEnabled(true);
+            } 
+        }, delay_ms)
     }
 }
